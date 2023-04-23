3f0715373d2a2f2c29a05c9bed0582c6e73207e4
martialtimira <zignar1999@gmail.com>
2023-04-23 04:19:53 +0200

    Initial implementation (need to make update method so only 1 visor opens) and more stuff

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,41 @@
+import java.awt.image.BufferedImage;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import javax.imageio.ImageIO;
+
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void main(String[] args) throws IOException {
+
+        String zipFilePath = "Cubo.zip";
+
+        try (ZipInputStream zis = new ZipInputStream(new FileInputStream(zipFilePath))) {
+
+            ZipEntry entry;
+            while ((entry = zis.getNextEntry()) != null) {
+
+                // Només llegim les entrades que són fitxers
+                if (!entry.isDirectory()) {
+
+                    // Creem un objecte BufferedImage a partir de l'entrada ZIP
+                    BufferedImage image = ImageIO.read(zis);
+
+                    // Mostrem la imatge
+                    Visor visor = new Visor(image);
+                    visor.setVisible(true);
+
+                }
+
+                // Indiquem que hem acabat de llegir aquesta entrada
+                zis.closeEntry();
+
+            }
+
+        }
+
     }
+
 }
